7d38e0eccf443c85b00e874c7b78c8e3fe46eafd
Wesley Rodrigues da Silva <wesley.it@gmail.com>
2015-10-03 11:26:37 -0300

    Fixed the image name on run.sh script

--- ws-apache2/run.sh
+++ ws-apache2/run.sh
@@ -4,8 +4,7 @@ docker rm -f ws-apache2 2> /dev/null
 docker run -ti -d \
 	--name ws-apache2 \
 	--hostname ws-apache2.docker.local \
-	-p 80:80 -p 8080:8080 \
-	-p 443:443 -p 8443:8443 \
+	-p 80:80 \
 	-v `pwd`/sites-enabled:/etc/apache2/sites-enabled \
 	-v `pwd`/www:/srv/www \
-	ws-apache2
+	wesleyit/ws-apache2
